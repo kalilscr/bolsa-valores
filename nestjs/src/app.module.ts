9d3c94db6b43648da494bfdfc5351578680ca3d1
root <root@Kalil>
2024-02-06 11:01:52 -0300

    feat: finishes nestjs

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AssetsModule } from './assets/assets.module';
+import { WalletsModule } from './wallets/wallets.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, AssetsModule, WalletsModule, OrdersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ nestjs/src/orders/order.dto.ts
@@ -0,0 +1,19 @@
+import { OrderType } from '@prisma/client';
+
+export class InitTransactionDto {
+  asset_id: string;
+  wallet_id: string;
+  shares: number;
+  price: number;
+  type: OrderType;
+}
+//class-transformer class-validator
+
+export class InputExecuteTransactionDto {
+  order_id: string;
+  status: 'OPEN' | 'CLOSED';
+  related_investor_id: string;
+  broker_transaction_id: string;
+  negotiated_shares: number;
+  price: number;
+}
